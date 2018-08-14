19d178e3e64ccb829fa4e7c541ce4dd5c7cdf5c3
Fergus Byrne <fbyrne@murex.com>
2018-08-14 17:50:44 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # qpid-dispatch-jms
 Testing Apache Qpid Dispatcher with JMS Client
+
+```bat
+docker-compose build
+docker-compose -d up
+
+docker exec broker1 curl --user admin --pass admin -X PUT  -d '{"durable":true}' http://localhost:80/api/latest/queue/default/default/requests
+docker exec broker2 curl --user admin --pass admin -X PUT  -d '{"durable":true}' http://localhost:80/api/latest/queue/default/default/requests
+docker exec dispatcher qdstat -c
+docker exec dispatcher qdstat --autolinks
+```
+
+```bat
+docker-compose stop
+docker-compose start
+docker-compose restart
+docker exec 
+```
---
+++ jms-test/src/test/java/org/apache/qpid/dispatch/jmstest/RequestResponseReplyToTest.java
@@ -0,0 +1,104 @@
+package org.apache.qpid.dispatch.jmstest;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.jms.Connection;
+import javax.jms.ConnectionFactory;
+import javax.jms.Destination;
+import javax.jms.ExceptionListener;
+import javax.jms.JMSException;
+import javax.jms.Message;
+import javax.jms.MessageConsumer;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+import javax.jms.TemporaryQueue;
+import javax.jms.TextMessage;
+import javax.naming.Context;
+import javax.naming.InitialContext;
+
+
+public class RequestResponseReplyToTest {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(RequestResponseReplyToTest.class);
+
+	@Test
+	public void send_request_with_reply_to() throws Exception {
+		// The configuration for the Qpid InitialContextFactory has been supplied in
+		// a jndi.properties file in the classpath, which results in it being picked
+		// up automatically by the InitialContext constructor.
+		Context context = new InitialContext();
+
+		ConnectionFactory factory = (ConnectionFactory) context.lookup("dispatcherFactoryLookup");
+		Destination queue = (Destination) context.lookup("requests");
+
+		try(Connection connection = factory.createConnection()) {
+			connection.setExceptionListener(new MyExceptionListener());
+			connection.start();
+
+            try (Session session1 = connection.createSession()) {
+                TemporaryQueue responseQueue = session1.createTemporaryQueue();
+
+                try (MessageProducer requestProducer = session1.createProducer(queue)) {
+                    TextMessage message = session1.createTextMessage("Hello world!");
+                    message.setJMSReplyTo(responseQueue);
+                    requestProducer.send(message);
+                    LOGGER.info("Sent request:");
+                    logDetails(message);
+                }
+
+                try (Session session2 = connection.createSession()) {
+                    try (MessageConsumer messageConsumer1 = session2.createConsumer(queue)) {
+                        TextMessage message1 = (TextMessage) messageConsumer1.receive(10000);
+                        if(message1 == null){
+                            throw new RuntimeException("no message received");
+                        }
+
+                        LOGGER.info("Received request:");
+                        logDetails(message1);
+
+                        Destination replyTo = message1.getJMSReplyTo();
+                        //Destination replyTo = responseQueue;
+                        try (MessageProducer messageProducer = session2.createProducer(replyTo)) {
+                            TextMessage message = session2.createTextMessage("Hello world response!");
+
+                            LOGGER.info(String.format("Sending response: %s", replyTo));
+                            messageProducer.send(message);
+
+                            LOGGER.info("Sent response:");
+                            logDetails(message1);
+                        }
+                    }
+                }
+
+                try (MessageConsumer messageConsumer1 = session1.createConsumer(responseQueue)) {
+                    TextMessage receivedResponse = (TextMessage) messageConsumer1.receive(10000);
+                    if(receivedResponse == null){
+                        throw new RuntimeException("no message received!");
+                    }
+                    LOGGER.info("Received response:");
+                    logDetails(receivedResponse);
+                }
+            }
+		}
+	}
+
+    private void logDetails(Message receivedMessage) throws JMSException {
+        LOGGER.info(String.format("\tMessageID:%s", receivedMessage.getJMSMessageID()));
+        LOGGER.info(String.format("\tCorrelationID:%s", receivedMessage.getJMSCorrelationID()));
+        LOGGER.info(String.format("\tTimestamp:%s", receivedMessage.getJMSTimestamp()));
+        LOGGER.info(String.format("\tExpiration:%s", receivedMessage.getJMSExpiration()));
+        LOGGER.info(String.format("\tDeliveryTime:%s", receivedMessage.getJMSDeliveryTime()));
+        LOGGER.info(String.format("\tReply-To:%s", receivedMessage.getJMSReplyTo()));
+        LOGGER.info(String.format("\tBody:%s", receivedMessage.getBody(String.class)));
+    }
+
+    private static class MyExceptionListener implements ExceptionListener {
+		@Override
+		public void onException(JMSException exception) {
+            LOGGER.error("Connection ExceptionListener fired, exiting.", exception);
+		}
+	}
+
+}

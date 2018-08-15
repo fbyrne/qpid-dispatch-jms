195ef61d4fef5f2e33129023059b06a704fdf652
Fergus Byrne <fbyrne@murex.com>
2018-08-15 13:00:42 +0100

    Add separate tests for listener and receive.  Move requestHandler to async thread

--- jms-test/src/test/java/org/apache/qpid/dispatch/jmstest/RequestResponseReplyToTest.java
+++ jms-test/src/test/java/org/apache/qpid/dispatch/jmstest/RequestResponseReplyToTest.java
@@ -1,11 +1,13 @@
 package org.apache.qpid.dispatch.jmstest;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import javax.jms.Connection;
 import javax.jms.ConnectionFactory;
+import javax.jms.DeliveryMode;
 import javax.jms.Destination;
 import javax.jms.ExceptionListener;
 import javax.jms.JMSException;
@@ -17,22 +19,61 @@ import javax.jms.TemporaryQueue;
 import javax.jms.TextMessage;
 import javax.naming.Context;
 import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import java.util.concurrent.atomic.AtomicReference;
 
 public class RequestResponseReplyToTest {
 
     private static final Logger LOGGER = LoggerFactory.getLogger(RequestResponseReplyToTest.class);
 
-    @Test
-    public void send_request_with_reply_to() throws Exception {
+    private ConnectionFactory connectionFactory;
+    private Destination requestQueue;
+
+    @BeforeEach
+    public void setup() throws NamingException {
         // The configuration for the Qpid InitialContextFactory has been supplied in
         // a jndi.properties file in the classpath, which results in it being picked
         // up automatically by the InitialContext constructor.
         Context context = new InitialContext();
 
-        ConnectionFactory factory = (ConnectionFactory) context.lookup("dispatcherFactoryLookup");
-        Destination queue = (Destination) context.lookup("requests");
+        this.connectionFactory = (ConnectionFactory) context.lookup("dispatcherFactoryLookup");
+        this.requestQueue = (Destination) context.lookup("requests");
+    }
 
-        try (Connection connection = factory.createConnection()) {
+    @Test
+    public void request_response_with_reply_to_listener() throws Exception {
+        try (Connection connection = connectionFactory.createConnection()) {
+            connection.setExceptionListener(new MyExceptionListener());
+            connection.start();
+
+            try (Session session1 = connection.createSession()) {
+                TemporaryQueue responseQueue = session1.createTemporaryQueue();
+                LOGGER.info("Temporary Response Queue: " + responseQueue);
+
+                try (MessageConsumer responseConsumer = session1.createConsumer(responseQueue)) {
+                    AtomicReference<Message> receivedResponse = new AtomicReference<>();
+                    responseConsumer.setMessageListener(receivedResponse::set);
+                    LOGGER.info("Temporary Response Queue Consumer Created: " + responseQueue);
+
+                    new Thread(this::responseHandler, "Response Handler").start();
+                    Thread.sleep(2000);
+
+                    sendRequest(session1, responseQueue);
+
+                    Thread.sleep(10000);
+                    if (receivedResponse.get() == null) {
+                        throw new RuntimeException("no message received!");
+                    }
+                    LOGGER.info("Received response:");
+                    logDetails(receivedResponse.get());
+                }
+            }
+        }
+    }
+
+    @Test
+    public void request_response_with_reply_to_receive() throws Exception {
+        try (Connection connection = connectionFactory.createConnection()) {
             connection.setExceptionListener(new MyExceptionListener());
             connection.start();
 
@@ -43,43 +84,12 @@ public class RequestResponseReplyToTest {
                 try (MessageConsumer responseConsumer = session1.createConsumer(responseQueue)) {
                     LOGGER.info("Temporary Response Queue Consumer Created: " + responseQueue);
 
-                    try (MessageProducer requestProducer = session1.createProducer(queue)) {
-                        TextMessage message = session1.createTextMessage("Hello world!");
-                        message.setJMSReplyTo(responseQueue);
-                        requestProducer.send(message);
-                        LOGGER.info(String.format("Sent request: %s", queue));
-                        logDetails(message);
-                    }
+                    new Thread(this::responseHandler, "Response Handler").start();
+                    Thread.sleep(2000);
 
-                    try (Session session2 = connection.createSession()) {
-                        try (MessageConsumer requestConsumer = session2.createConsumer(queue)) {
-                            LOGGER.info("Request Queue Consumer Created: " + queue);
-                            TextMessage message1 = (TextMessage) requestConsumer.receive(10000);
-                            if (message1 == null) {
-                                throw new RuntimeException("no message received");
-                            }
+                    sendRequest(session1, responseQueue);
 
-                            LOGGER.info("Received request:");
-                            logDetails(message1);
-
-                            Destination replyTo = message1.getJMSReplyTo();
-                            // Destination replyTo = responseQueue; // TEST by setting response queue directly
-                            try (MessageProducer messageProducer = session2.createProducer(replyTo)) {
-                                LOGGER.info("Temporary Response Queue Producer Created: " + replyTo);
-                                TextMessage message = session2.createTextMessage("Hello world response!");
-
-                                LOGGER.info(String.format("Sending response: %s", replyTo));
-                                logDetails(message);
-                                messageProducer.send(message);
-
-                                LOGGER.info("Sent response:");
-                                logDetails(message);
-                            }
-                        }
-
-                    }
-
-                    TextMessage receivedResponse = (TextMessage) responseConsumer.receive(10000);
+                    Message receivedResponse = responseConsumer.receive(10000);
                     if (receivedResponse == null) {
                         throw new RuntimeException("no message received!");
                     }
@@ -87,6 +97,56 @@ public class RequestResponseReplyToTest {
                     logDetails(receivedResponse);
                 }
             }
+        }
+    }
+
+    private void sendRequest(Session session, TemporaryQueue responseQueue) throws JMSException {
+        try (MessageProducer requestProducer = session.createProducer(requestQueue)) {
+            TextMessage message = session.createTextMessage("Hello world!");
+            message.setJMSReplyTo(responseQueue);
+            message.setJMSExpiration(10000l);
+            requestProducer.send(message);
+            LOGGER.info(String.format("Sent request: %s", requestQueue));
+            logDetails(message);
+        }
+    }
+
+    private void responseHandler() {
+        try {
+            try (Connection connection = connectionFactory.createConnection()) {
+                connection.setExceptionListener(new MyExceptionListener());
+                connection.start();
+                try (Session session2 = connection.createSession()) {
+                    try (MessageConsumer requestConsumer = session2.createConsumer(requestQueue)) {
+                        LOGGER.info("Request Queue Consumer Created: " + requestQueue);
+                        TextMessage receivedMessage = (TextMessage) requestConsumer.receive(20000);
+                        if (receivedMessage == null) {
+                            throw new RuntimeException("no message received");
+                        }
+
+                        LOGGER.info("Received request:");
+                        logDetails(receivedMessage);
+
+                        Destination replyTo = receivedMessage.getJMSReplyTo();
+                        // TEST by setting response queue directly
+                        // Destination replyTo = responseQueue;
+                        try (MessageProducer messageProducer = session2.createProducer(replyTo)) {
+                            LOGGER.info("Temporary Response Queue Producer Created: " + replyTo);
+                            TextMessage message = session2.createTextMessage("Hello world response!");
+//                            message.setJMSDeliveryMode(DeliveryMode.NON_PERSISTENT);
+
+                            LOGGER.info(String.format("Sending response: %s", replyTo));
+                            logDetails(message);
+                            messageProducer.send(message);
+
+                            LOGGER.info("Sent response:");
+                            logDetails(message);
+                        }
+                    }
+                }
+            }
+        } catch (Exception e) {
+            LOGGER.error("failed to handle response", e);
         }
     }
 
